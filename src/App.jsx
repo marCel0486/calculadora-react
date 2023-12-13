c15e36b45d85f55b31b0576d9776153a8096fffb
marcelo04 <marceloqueiroz7894@gmail.com>
2023-12-13 14:43:20 -0300

    calculadora 1.0

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './App.css'
+import Calculator from './componentes/Calculator'
+
+function App() {
+  return(
+    <>
+    <Calculator/>
+    </>
+  )
+}
+
+export default App
